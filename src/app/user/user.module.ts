80cb1e303947852c9cd1f313aec0f432782abf9e
joe-bookwood <github@buchholz-it.de>
2021-06-09 09:59:00 +0200

    user store

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromUser from './store/user.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { UserEffects } from './store/user.effects';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(fromUser.userFeatureKey, fromUser.reducer),
+    EffectsModule.forFeature([UserEffects])
+  ]
+})
+export class UserModule { }
