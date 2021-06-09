1e980f00fc6c44e5b2f3a05f07bfbf505c5ee6df
joe-bookwood <github@buchholz-it.de>
2021-06-09 22:00:41 +0200

    2 files modified, 2 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { UsersComponent } from './users/users.component';
 import { UserService } from './user.service';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, UsersComponent ],
+  declarations: [ AppComponent, HelloComponent, UserDetailComponent ],
   bootstrap:    [ AppComponent ],
   providers: [UserService]
 })
