678b8228c7325693efe666b13fbf68a7ca700186
joe-bookwood <github@buchholz-it.de>
2021-06-14 22:05:14 +0200

    2 files modified, 4 deleted

--- src/app/user/store/user.reducer.ts
+++ src/app/user/store/user.reducer.ts
@@ -1,14 +1,15 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { IUser } from '../../model/user.model';
 import * as UserActions from './user.actions';
 
 export const userFeatureKey = 'user';
 
 export interface State {
-
+  user: IUser | undefined
 }
 
 export const initialState: State = {
-
+  user: undefined
 };
 
 
